9c838b44fe9ba05844b304de7dd0c02be3842635
Andy Cheng <andy810@connect.hku.hk>
2025-05-29 01:19:47 +0800

    Enhance project configuration and backend functionality. Added start script to package.json, updated .env.example with NODE_ENV, and improved backend index.ts to serve static files in production. Updated axios base URL configuration for development and production environments.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,23 +4,35 @@ import cors from "cors";
 import notesRoute from "./routes/notes.routes.js";
 import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 // Routes
 app.use("/api/v1/notes", notesRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*path", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api/v1"
+    : "/api/v1";
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL: BASE_URL,
 });
 
 export default api;
